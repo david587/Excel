a0bf72a0227e0e56fedbb9bd2e7279c0dc91e0c1
David <davidbarath08@gmail.com>
2023-02-08 15:38:10 +0100

    tailwind hozzáadva, excel könyvtár használta még sikeretlen, verzióbeli probléma miatt

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "black":"#20202a",
+        "light-black":"#2C2C3A",
+        "gray":"#F0F0F0",
+        "green": "#2CA58D",
+      },
+    },
+  },
+  plugins: [],
+}
